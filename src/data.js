c7a0c7b0a71ac1f62645926ec7aa8b5c098a81e4
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-01-23 20:27:07 -0300

    Add posts data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "JAPAN",
+    googleMapUrl: "https://maps.app.goo.gl/ewUxrJEa2RwVaP5A9",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "Rectangle 77.png"
+  },
+  {
+    title: "Sydney Opera House",
+    location: "AUSTRALIA",
+    googleMapUrl: "https://maps.app.goo.gl/9PeZSvqz9m5MqpVw7",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    imageUrl: "Rectangle 78.png"
+  },
+  {
+    title: "Geirangerfjord",
+    location: "NORWAY",
+    googleMapUrl: "https://maps.app.goo.gl/VcQL11utmsi12mdJ9",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl: "Rectangle 84.png"
+  },
+]
